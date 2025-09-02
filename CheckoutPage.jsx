add4c7b0ef4e22eaff00dabfa5fc196cf3449926
michaelmorks <s224437209@deakin.edu.au>
2025-09-02 14:12:40 +1000

    Create CheckoutPage.jsx

---
+++ CheckoutPage.jsx
@@ -0,0 +1,8 @@
+export default function CheckoutPage() {
+  return (
+    <div className="page text-center">
+      <h2>Checkout</h2>
+      <p>Stripe payment integration will go here 🚀</p>
+    </div>
+  );
+}
